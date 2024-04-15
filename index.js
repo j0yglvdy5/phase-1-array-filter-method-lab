d04db5b101ebf29b0abe5e06e7129e913919c0cd
j0yglvdy5 <joygladys.njeri@student.moringaschool.com>
2024-04-15 17:45:26 +0300

    phase 1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+
+const drivers = [
+    { name: 'Bobby' },
+    { name: 'Sammy' },
+    { name: 'Sally' },
+    { name: 'Annette' },
+    { name: 'Bobby' }
+  ];
+  function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.name && driver.name.toLowerCase() === name.toLowerCase())
+                  .map(driver => driver.name);
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.name && driver.name.toLowerCase().startsWith(letters.toLowerCase()))
+                  .map(driver => driver.name);
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name)
+                  .map(driver => driver.name);
+  }
+  
+
+  
+  
